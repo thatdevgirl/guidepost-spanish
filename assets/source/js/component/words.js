ba8208a626de6430a180786a9cc1f172d1c4e0d1
Joni Halabi <her@thatdevgirl.com>
2021-05-23 21:05:58 -0400

    Adding recent word swap; updating dependencies

--- assets/source/js/component/words.js
+++ assets/source/js/component/words.js
@@ -7,14 +7,26 @@
 const Words = ( () => {
 
   // Make a call to get our list of vocabulary words from the local JSON file.
-  $.getJSON( '/data/words.json', ( data ) => {
+  $.getJSON( '/data/words.json', ( allWords ) => {
+
+    /**
+     * Generate a list of only the most recent words.
+     */
+    var recentWords = allWords.filter( word => word.most_recent == 1 );
+
+
+    /**
+     * Initialize the list of words to all the words.
+     */
+    var data = allWords;
+
 
     /**
      * getNextWord()
      *
      * Randomly get the next vocabulary word.
      */
-    const getNextWord = ( currentWord=0 ) => {
+    const getNextWord = ( currentWord, data ) => {
       // Generate a random number between 1 and the total number of words.
       let rand = Math.random() * data.length;
 
@@ -24,7 +36,7 @@ const Words = ( () => {
       // the random number down.
       const word = ( Math.floor(rand) == currentWord ) ? Math.ceil(rand) : Math.floor(rand);
 
-      toggle( word, false );
+      toggle( word, false, data );
       return word;
     }
 
@@ -34,7 +46,7 @@ const Words = ( () => {
      *
      * Toggle (show/hide) the passed-in word in the appropriate language.
      */
-    const toggle = ( word, swap=true ) => {
+    const toggle = ( word, swap=true, data ) => {
       // Default the language to Spanish.
       let lang = 'es';
 
@@ -52,7 +64,7 @@ const Words = ( () => {
         }
       }
 
-      // Show the word in the correct language.g
+      // Show the word in the correct language.
       $( '#vocabulary-word' ).html( data[word][lang] ).attr( 'lang', lang );
     }
 
@@ -66,15 +78,45 @@ const Words = ( () => {
      * When the user presses the return key, a new word is randomly chosen.
      */
     $( document ).keypress( (e) => {
-      if ( e.which == 32 ) { toggle( currentWord ); }       // Space bar.
-      if ( e.which == 13 ) { currentWord = getNextWord(); } // Return key.
+      if ( e.which == 32 ) { toggle( currentWord, true, data ); }              // Space bar.
+      if ( e.which == 13 ) { currentWord = getNextWord( currentWord, data ); } // Return key.
     } );
 
 
     /**
      * Single click event: Language toggles between Spanish and English.
      */
-    $( document ).click( () => { toggle( currentWord ); } );
+    $( 'main' ).click( () => { toggle( currentWord, true, data ); } );
+
+
+    /**
+     * Button click events: Toggle the set of words to be used.
+     */
+    $( 'button#all-words' ).click( () => {
+      // Change the display of the word set buttons.
+      $( '#all-words' ).addClass('selected');
+      $( '#recent-words' ).removeClass('selected');
+
+      // Update the set of words to use to all words and then swap the words
+      // to make the functionality here consistent with the switch to recent
+      // words only. The word swap is not required here because recent words
+      // will always be part of all words, but having the same behavior is nice.
+      data = allWords;
+      currentWord = getNextWord( currentWord, data );
+    } );
+
+    $( 'button#recent-words' ).click( () => {
+      // Change the display of the word set buttons.
+      $( '#all-words' ).removeClass('selected');
+      $( '#recent-words' ).addClass('selected');
+
+      // Update the set of words to use to only recent words and then swap
+      // words to make sure the current word is in this set of recent words.
+      // An error occurs if this swap happens and the current word is not in the
+      // set of recent words.
+      data = recentWords;
+      currentWord = getNextWord( currentWord, data );
+    } );
 
 
     /**
@@ -82,11 +124,11 @@ const Words = ( () => {
      */
     var hammertime = new Hammer( document );
     hammertime.get( 'pan' ).set({ direction: Hammer.DIRECTION_ALL, threshold: 50 });
-    hammertime.on( 'panend', () => { currentWord = getNextWord(); } );
+    hammertime.on( 'panend', () => { currentWord = getNextWord( currentWord, data ); } );
 
 
     // Run this puppy!
-    let currentWord = getNextWord();
+    let currentWord = getNextWord( 0, data );
 
   } );
 
